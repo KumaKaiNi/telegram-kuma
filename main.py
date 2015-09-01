ad6976b87089ca0e00faf10894c22888e08c830f
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-01 12:38:42 -0400

    created dice exception

--- main.py
+++ main.py
@@ -239,21 +239,25 @@ def send_coinflip (m):
 # Rolls dice.
 @bot.message_handler(commands=['roll'])
 def send_diceroll (m):
-   dice = m.text.split()[1]
-   mult = m.text.split()[1].split('d')
-   if len(m.text.split()) >= 3:
-      out = "Please only send one input."
-   elif len(mult) == 2:
-      out = "Rolled " 
-      for x in range (0, int(mult[0])):
-         out += str(random.randint(0, int(mult[1])))
-         out += ", "
-      out = out[:-1] # Remove [ ]
-      out = out[:-1] # Remove [,]
-   else:
-      out = "Rolled a " + str(random.randint(0,int(dice)))
-   bot.send_message(m.chat.id, out)
-   print('[MSG]', out)
+   try:
+      dice = m.text.split()[1]
+      mult = m.text.split()[1].split('d')
+      if len(m.text.split()) >= 3:
+         out = "Please only send one input."
+      elif len(mult) == 2:
+         out = "Rolled " 
+         for x in range (0, int(mult[0])):
+            out += str(random.randint(0, int(mult[1])))
+            out += ", "
+         out = out[:-1] # Remove [ ]
+         out = out[:-1] # Remove [,]
+      else:
+         out = "Rolled a " + str(random.randint(0,int(dice)))
+   except:
+      out = "Please send only numbers."
+   finally:
+      bot.send_message(m.chat.id, out)
+      print('[MSG]', out)
 
 
 # Returns a greeting if a user starts a sentence with the following regex.
