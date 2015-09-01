ccbe6f86435a21d37db4886f54b95bae17476367
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-01 12:47:27 -0400

    dsjkflsad

--- main.py
+++ main.py
@@ -245,8 +245,8 @@ def send_diceroll (m):
       if len(m.text.split()) >= 3:
          out = "Please only send one input."
       elif len(mult) == 2:
-         if int(mult[0]) <= 0 or int(mult[1]) <= 0:
-            out = "Inputs must be 1 or more."
+         if int(mult[0]) <= 1 or int(mult[1]) <= 1:
+            out = "Inputs must be 2 or more. Example: 4d6"
          else:
             out = "Rolled " 
             for x in range (0, int(mult[0])):
