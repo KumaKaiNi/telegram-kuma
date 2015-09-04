644a5157de78f0bc619dc11390b2a66435d55c35
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-04 01:20:18 -0600

    renamed some lists for consistency

--- main.py
+++ main.py
@@ -316,11 +316,11 @@ Regex listeners.
 
 
 # Returns a greeting if a user starts a sentence with the following regex.
-GREETINGS = ['sup loser', 'yo', 'ay', 'go away', 'hi', 'wassup']
-SEARCH_FOR = ['hi', 'hello', 'yo', 'sup']
-@bot.message_handler(regexp=to_regex(SEARCH_FOR,'','()|( ).*'))
+GREETS = ['hi', 'hello', 'yo', 'sup']
+GREET_REPLIES = ['sup loser', 'yo', 'ay', 'go away', 'hi', 'wassup']
+@bot.message_handler(regexp=to_regex(GREETS,'','()|( ).*'))
 def send_hello (m):
-   out = GREETINGS[random.randint(0, len(GREETINGS) - 1)]
+   out = GREETINGS[random.randint(0, len(GREET_REPLIES) - 1)]
    bot.send_message(m.chat.id, out)
    print(CON['msg'], out)
 
