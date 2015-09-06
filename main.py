c67947082318fefc0c381107e2a255bbee68f3ac
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-06 17:23:01 -0600

    log fixing

--- main.py
+++ main.py
@@ -48,13 +48,19 @@ def log (m):
       logging.info(m.text)
 
 def word_log (m):
-   logging.basicConfig(
-      filename = './wordlogs/' + str(m.chat.id) + '.log',
-      filemode = 'a+',
-      format = '%(message)s')
-   # Ignores messages that start with links or are just links
-   if m.text.split(':')[0] not in ['http', 'https']:
-      logging.info(m.text)
+   try:
+      open('./wordlogs/' + str(m.chat.id) + '.log', 'a+', encoding='utf8')
+   except:
+      print(CON['err'], 'Log file does not exist. Creating.')
+      open('./wordlogs/' + str(m.chat.id) + '.log', 'w', encoding='utf8')
+   finally:
+      logging.basicConfig(
+         filename = './wordlogs/' + str(m.chat.id) + '.log',
+         filemode = 'a+',
+         format = '%(message)s')
+      # Ignores messages that start with links or are just links
+      if m.text.split(':')[0] not in ['http', 'https']:
+         logging.info(m.text)
 
 
 # Function to populate top posts of a given subreddit
