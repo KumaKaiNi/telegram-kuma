cc9976d1dea6f3c0e17a060d31cad37287008f6c
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-06 21:10:36 -0600

    testing

--- main.py
+++ main.py
@@ -346,12 +346,18 @@ def send_rights (m):
 
 
 # Prints available json to the console.
-@bot.message_handler(commands=['test'])
+@bot.message_handler(commands=['json'])
 def print_json (m):
    if m.from_user.username == "rekyuu":
       print(CON['log'], 'info:', m.__dict__)
       print(CON['log'], 'from_user:', m.from_user.__dict__)
       print(CON['log'], 'chat:', m.chat.__dict__)
+
+
+# Prints available json to the console.
+@bot.message_handler(commands=['test'])
+def testing_function (m):
+   get_top_posts('warshipporn')
 
 
 """
