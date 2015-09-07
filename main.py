4935d10f1f3c72a9c4d1427bcae544c25029130d
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-06 21:49:12 -0600

    testing

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ def send_dank (m):
    name = m.from_user.username
    if name is None:
       name = m.from_user.first_name
-   out = ''.join(['Shut the fuck up, @', str(name)])
+   out = ''.join(['Shut the fuck up, @', name])
    bot.send_message(m.chat.id, out)
    print(CON['msg'], out)
 
