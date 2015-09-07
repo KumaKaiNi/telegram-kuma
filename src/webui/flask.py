dd45832f8f3f5d395a287fa17af5555b6df6417e
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-07 02:02:59 -0600

    webui testing

--- src/webui/flask.py
+++ src/webui/flask.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+
+users = {'rekyuu': 'password'}
+
+@auth.get_password
+def get_pw (username):
+   if username in users:
+      return users.get(username)
+   return None
 
 
 @app.route('/')
-def hello ():
-   return "Hello, kuma!"
+@auth.login_required
+def index ():
+   return "Hello, %s!" % auth.username()
 
 
 app.run(port = 5268, debug = False, threaded = True)
