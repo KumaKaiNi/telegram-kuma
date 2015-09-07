7fa21c17ade71472122a0cdf7e179a164e55d6cb
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-07 00:32:32 -0600

    adding webui

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import threading, time
 
-# from webui import flask
+from webui import flask
 
 
 def core_worker():
    from core import kuma
 
 def webui_worker():
-   print("Test worker thread started.")
-   return
+   from webui import flask
 
 
 if __name__ == '__main__':
