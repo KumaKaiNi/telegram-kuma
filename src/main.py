89d1c92d738bf93b8f0462c99ff9c90c44b5d615
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-07 00:57:39 -0600

    testing

--- src/main.py
+++ src/main.py
@@ -5,8 +5,11 @@ from webui import flask
 def core_worker():
    from core import kuma
 
+def webui_worker():
+   flask.app.run(port = 5268, debug = True, threaded = True)
+
 
 if __name__ == '__main__':
 
-   flask.app.run(port = 5268, debug = True, threaded = True)
+   threading.Thread(target = webui_worker).start()
    threading.Thread(target = core_worker).start()
