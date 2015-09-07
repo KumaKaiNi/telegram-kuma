67cb164d04b9b92a9d6527f91960a8f209de2f14
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-07 00:54:37 -0600

    testing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 import threading, time
+from webui import flask
 
 
 def core_worker():
    from core import kuma
 
-def webui_worker():
-   from webui import flask
-
 
 if __name__ == '__main__':
 
    threading.Thread(target = core_worker).start()
-   threading.Thread(target = webui_worker).start()
+   flask.app.run(port = 5268, debug = True, threaded = True)
--- src/webui/flask.py
+++ src/webui/flask.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello ():
    return "Hello, kuma!"
-
-
-app.run(port = 5268, debug = True, threaded = True)
