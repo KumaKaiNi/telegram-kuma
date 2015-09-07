2ddf88bd72bbaba25b38e0e42b316a98fccc9818
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-07 00:50:00 -0600

    testing

--- src/main.py
+++ src/main.py
@@ -1,24 +1,14 @@
 import threading, time
-from flask import Flask
-app = Flask(__name__)
 
-"""
-Worker definitions
-"""
 
 def core_worker():
    from core import kuma
 
-
-"""
-Flask settings
-"""
-
-@app.route('/')
-def hello ():
-   return "Hello, kuma!"
+def webui_worker():
+   from webui import flask
 
 
 if __name__ == '__main__':
+
    threading.Thread(target = core_worker).start()
-   app.run(port = 5268, debug = True, threaded = True)
+   threading.Thread(target = webui_worker).start()
--- src/webui/flask.py
+++ src/webui/flask.py
@@ -6,4 +6,4 @@ def hello ():
    return "Hello, kuma!"
 
 if __name__ == "__main__":
-   app.run(port = 5268, debug = True)
+   app.run(port = 5268, debug = True, threaded = True)
