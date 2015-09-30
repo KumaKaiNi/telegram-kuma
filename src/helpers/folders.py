3a22c5603ae69c16de754c0c2d11ab5965df1e57
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-29 23:21:53 -0600

    fixing

--- src/helpers/folders.py
+++ src/helpers/folders.py
@@ -1,6 +1,6 @@
 import json, os
 
-DIR = os.path.dirname(os.path.realpath(__file__ + '../'))
+DIR = os.path.dirname(os.path.realpath(__file__ + '/../'))
 FOLDER = DIR + '/__cache__/'
 CACHE = {
    'img': FOLDER + 'img/',
